86b3c7901e6e64a9d057611f3804bdac85ddde0b
allcario <alli@live.nl>
2026-08-01 15:17:50 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -20,7 +20,7 @@ import numpy as np
 import pandas as pd
 
 # ================= INSTELLINGEN =================
-SYMBOL = "ADA/USD"
+SYMBOL = "ETH/USD"
 TIMEFRAME = "1h"
 YEARS_BACK = 2
 RISK_PCT = 0.01      # 1% stop-loss
